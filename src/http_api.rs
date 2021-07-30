6f1532034366ceaaeeb4a57ff5468a853280d568
Quinn Stephan <quinn.stp@gmail.com>
2021-07-30 03:18:00 +0000

    http api error handling

--- src/http_api.rs
+++ src/http_api.rs
@@ -1,98 +1,72 @@
 use core::time::Duration;
-use iron::prelude::*;
 use serde::{Serialize, Deserialize};
 use glam::*;
+use warp::*;
 
 use crate::image::*;
-use crate::config::*;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PreviewRequest {
-    pub config: Config,
-    pub coord: IVec3,
+    pub tile_uri_format: String,
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
     pub min_val: f32,
     pub max_val: f32
 }
 
-fn hello_world(r: &mut Request) -> IronResult<Response> {
-    let a = r.url.query().ok_or((iron::status::BadRequest, "text/plain", "somehow getting the query string failed"))
-    .and_then(|s| urlencoding::decode(s).or(Err((iron::status::BadRequest, "text/plain", "query string encoding was malformed"))))
-    .and_then(|s| serde_json::from_str::<PreviewRequest>(&s).or(Err((iron::status::BadRequest, "text/plain", "failed to parse preview request struct"))));
+#[derive(Debug)]
+struct ReqwestError(reqwest::Error);
+impl warp::reject::Reject for ReqwestError {}
 
-    let preview_request = match a {
-        Ok(c) => c,
-        Err(m) => return Ok(Response::with(m))
-    };
+#[derive(Debug)]
+struct ImageDecodeError(String);
+impl warp::reject::Reject for ImageDecodeError {}
 
-    let encoding = preview_request.config.encoding.unwrap_or(ImageEncoding::srtm());
+#[derive(Debug)]
+struct PreviewGenerateError;
+impl warp::reject::Reject for PreviewGenerateError {}
 
-    let client = reqwest::blocking::Client::builder()
+#[derive(Debug)]
+struct UriFormatError(String);
+impl warp::reject::Reject for UriFormatError {}
+
+async fn hello_world(r: PreviewRequest) -> Result<impl warp::reply::Reply, warp::Rejection>{
+    let encoding = ImageEncoding::srtm();
+
+    let client = reqwest::Client::builder()
         .timeout(Duration::from_secs(999))
         .build().unwrap();
 
-    let uri = crate::uri_fmt!(&preview_request.config.tile_uri_format, {
-        "x" => preview_request.coord.x,
-        "y" => preview_request.coord.y,
-        "z" => preview_request.coord.z
-    }).unwrap();
+    let uri = crate::uri_fmt!(&r.tile_uri_format, {
+        "x" => r.x,
+        "y" => r.y,
+        "z" => r.z
+    }).map_err(|e| warp::reject::custom(UriFormatError(e)))?;
+    
+    let bytes = client
+        .get(uri)
+        .send().await.map_err(|e| warp::reject::custom(ReqwestError(e)))?
+        .error_for_status().map_err(|e| warp::reject::custom(ReqwestError(e)))?
+        .bytes().await.map_err(|e| warp::reject::custom(ReqwestError(e)))?;
 
-    let b = client.get(uri).send()
-            .or(Err((iron::status::BadGateway, "text/plain", "requesting resource from server failed")))
-        .and_then(|r| r.bytes().or(Err((iron::status::BadGateway, "text/plain", "requesting resource body from server failed"))))
-        .and_then(|i| Image::decode(&encoding, &i[..]).or(Err((iron::status::InternalServerError, "text/plain", "decoding the image bytes failed"))));
+    let image = Image::decode(&encoding, &bytes[..])
+        .map_err(|e| warp::reject::custom(ImageDecodeError(e)))?;
 
-    let image = match b {
-        Ok(i) => i,
-        Err(m) => return Ok(Response::with(m))
-    };
-
-    let png_mime: iron::mime::Mime = "image/png".parse().unwrap();
-
-    return match crate::preview::make_preview(&image, preview_request.min_val, preview_request.max_val) {
-        Some(png_bytes) => Ok(Response::with((iron::status::Ok, png_mime, png_bytes.data))),
-        None => Ok(Response::with((iron::status::Ok, "text/plain", "generating preview failed")))
-    };
-
-
-    // let req_res = match reqwest::blocking::get("https://spkit.org/datasets/remapped/222_144_000.hgt") {
-    //     Ok(res) => res,
-    //     Err(_) => return Ok(Response::with((iron::status::BadGateway, "text/plain", "requesting resource from server failed")))
-    // };
-
-    // let body = match req_res.bytes() {
-    //     Ok(body) => body,
-    //     Err(_) => return Ok(Response::with((iron::status::BadGateway, "text/plain", "requesting resource body from server failed")))
-    // };
-
-
-
-    // let d = c.and_then(|c| if c.config.encoding.is_some() { Ok(c) } else { Err((iron::status::BadRequest, "text/plain", "dataset config does not have an explicit encoding")) }); 
-
-    // let query_str = match r.url.query() {
-    //     Some(query_str) => query_str,
-    //     None => return Ok(Response::with((iron::status::BadRequest, "text/plain", "somehow getting the query string failed?")))
-    // };
-
-    // let decoded_query_str = match urlencoding::decode(query_str) {
-    //     Ok(decoded) => decoded,
-    //     Err(_) => return Ok(Response::with((iron::status::BadRequest, "text/plain", "query string encoding was malformed")))
-    // };
-
-    // let conf = match serde_json::from_str::<PreviewRequest>(&decoded_query_str) {
-    //     Ok(config) => config,
-    //     Err(_) => return Ok(Response::with((iron::status::BadRequest, "text/plain", "failed to parse preview request struct")))
-    // };
-    // 
-    // let image_encoding = match conf.config.encoding {
-    //     Some(encoding) => encoding,
-    //     None => return Ok(Response::with((iron::status::BadRequest, "text/plain", "dataset config does not have an explicit encoding")))
-    // };
-
+    let preview = crate::preview::make_preview(&image, r.min_val, r.max_val)
+        .ok_or(warp::reject::custom(PreviewGenerateError))?;
+    
+    Ok(warp::http::Response::builder()
+        .header("Content-Type", "image/png")
+        .body(preview.data))
 }
 
-pub fn run() {
-    let chain = Chain::new(hello_world);
-    // chain.link_before(ResponseTime);
-    // chain.link_after(ResponseTime);
-    Iron::new(chain).http("localhost:3000").unwrap();
+pub async fn run() {
+    let routes = warp::get()
+        .and(warp::query::<PreviewRequest>())
+        .and_then(hello_world);
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3000))
+        .await
 }
--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,18 @@ pub mod dataset_cache;
 pub mod http_api;
 pub mod uri_format;
 
-use crate::config::Config;
-use glam::*;
-use crate::image::ImageEncoding;
-
 #[tokio::main]
 async fn main() {
     let preview_request = http_api::PreviewRequest {
         min_val: 0.0,
         max_val: 400.0,
-        coord: ivec3(222, 144, 0),
-        config: Config {
-            tile_uri_format: "https://spkit.org/datasets/remapped/222_144_000.hgt".to_string(),
-            encoding: Some(ImageEncoding::srtm())
-        }
+        x: 222,
+        y: 144,
+        z: 0,
+        tile_uri_format: "https://67.175.122.81/datasets/remapped/222_144_000.hgt".to_string(),
     };
 
-    println!("{}", serde_json::to_string(&preview_request).unwrap());
+    println!("{}", serde_urlencoded::to_string(&preview_request).unwrap());
     
-    http_api::run();
+    http_api::run().await;
 }
