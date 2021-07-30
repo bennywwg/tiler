eec8d8068452be8314e97f862c3fbdfaac8c1565
Quinn Stephan <quinn.stp@gmail.com>
2021-07-30 23:01:38 +0000

    more api cleanup

--- src/http_api.rs
+++ src/http_api.rs
@@ -15,21 +15,29 @@ pub struct PreviewRequest {
     pub max_val: f32
 }
 
-#[derive(Debug)]
-struct ReqwestError(reqwest::Error);
-impl warp::reject::Reject for ReqwestError {}
+macro_rules! warp_reject {
+    ($name: ident) => {
+        #[derive(Debug)]
+        struct $name;
+        impl warp::reject::Reject for $name {}
+    };
+    ($type: ty as $name: ident) => {
+        #[derive(Debug)]
+        struct $name($type);
+        impl warp::reject::Reject for $name {}
+        impl std::ops::Deref for $name {
+            type Target = $type;
+            fn deref(&self) -> &<Self as std::ops::Deref>::Target {
+                &self.0
+            }
+        }
+    }
+}
 
-#[derive(Debug)]
-struct ImageDecodeError(String);
-impl warp::reject::Reject for ImageDecodeError {}
-
-#[derive(Debug)]
-struct PreviewGenerateError;
-impl warp::reject::Reject for PreviewGenerateError {}
-
-#[derive(Debug)]
-struct UriFormatError(String);
-impl warp::reject::Reject for UriFormatError {}
+warp_reject!(PreviewGenerateError);
+warp_reject!(String as UriFormatError);
+warp_reject!(String as ImageDecodeError);
+warp_reject!(reqwest::Error as ReqwestError);
 
 async fn hello_world(r: PreviewRequest) -> Result<impl warp::reply::Reply, warp::Rejection>{
     let encoding = ImageEncoding::srtm();
@@ -42,19 +50,19 @@ async fn hello_world(r: PreviewRequest) -> Result<impl warp::reply::Reply, warp:
         "x" => r.x,
         "y" => r.y,
         "z" => r.z
-    }).map_err(|e| warp::reject::custom(UriFormatError(e)))?;
+    }).map_err(|e| UriFormatError(e))?;
     
     let bytes = client
         .get(uri)
-        .send().await.map_err(|e| warp::reject::custom(ReqwestError(e)))?
-        .error_for_status().map_err(|e| warp::reject::custom(ReqwestError(e)))?
-        .bytes().await.map_err(|e| warp::reject::custom(ReqwestError(e)))?;
+        .send().await.map_err(|e| ReqwestError(e))?
+        .error_for_status().map_err(|e| ReqwestError(e))?
+        .bytes().await.map_err(|e| ReqwestError(e))?;
 
     let image = Image::decode(&encoding, &bytes[..])
-        .map_err(|e| warp::reject::custom(ImageDecodeError(e)))?;
+        .map_err(|e| ImageDecodeError(e))?;
 
     let preview = crate::preview::make_preview(&image, r.min_val, r.max_val)
-        .ok_or(warp::reject::custom(PreviewGenerateError))?;
+        .ok_or(PreviewGenerateError)?;
     
     Ok(warp::http::Response::builder()
         .header("Content-Type", "image/png")
